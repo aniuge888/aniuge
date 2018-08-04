f2d5c1227387d592d3c0bb285a041988a8f07a0c
aniuge <2022677678@qq.com>
2018-08-04 08:43:40 +0800

    2

--- day01/src/Date时间类/Test6.java
+++ day01/src/Date时间类/Test6.java
@@ -5,5 +5,6 @@ public class Test6 {
         char [] chars = {'i','t','c','a','s','a'};
         System.arraycopy(chars,1,chars,5,1);
         System.out.println(chars);
+		System.out.println(chars);
     }
 }
